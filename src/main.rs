8ad3b85268484e3feaa38d569c5646550af645cc
torcuata22 <97114668+torcuata22@users.noreply.github.com>
2025-02-11 21:41:11 -0500

    strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string()); //new is an associated function, we call it directly on struct type, not an instance of that particular sruct type, use :: to access associated functions
+    server.run();
+}
+
+//Seerver is a  struct, a data type that lets you group together related data
+//create a struct:
+struct Server {
+    addr: String,
+}
+//to add functionality, write implementation block:
+impl Server {
+    fn new(addr: String) -> Self {
+        //returns a new instance of Server
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
 }
